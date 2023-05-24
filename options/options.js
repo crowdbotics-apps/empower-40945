f48b3fd3cc26c32fab3767bb030547eb0ad4cc70
Crowdbotics <team@crowdbotics.com>
2023-05-24 02:03:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "empower-40945",
+  url: "https://empower-40945.botics.co",
+  api: "https://empower-40945.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
